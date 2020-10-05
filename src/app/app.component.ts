384009e92d226f86bcf807ada35df5251bcdb0ea
Umesh Chandra  Vankadhara <umeshchandra.vankadhara@gmail.com>
2020-10-05 12:56:56 +0530

    capstone project

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+//import {AppDataService} from './_shared/_services/app-data.service';
+import { faSearch, faShoppingCart, faUser } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'upgrad-eshop-frontend';
+  faShoppingCart = faShoppingCart;
+  faUserIcon= faUser;
+  faSearchIcon =faSearch;
+
+  constructor() {
+    
+ }
+//  constructor( private appDataService: AppDataService) {}
+
+
+  ngOnInit(): void {
+  //  this.appDataService.initializeApp();
+  }
+
+  closeSubMenus() {
+
+  }
+}
