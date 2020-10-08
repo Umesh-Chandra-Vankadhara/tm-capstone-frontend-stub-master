be2ab27ffd7578a41549ce94e67f24349ce095cf
Umesh Chandra  Vankadhara <umeshchandra.vankadhara@gmail.com>
2020-10-08 15:27:14 +0530

    capstone project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home/home.component';
+import { OrdersComponent } from './order/orders/orders.component';
 import { ProductsComponent } from './product/products/products.component';
 
 
 const routes: Routes = [
-  // {path:'',redirectTo:"/products" ,component:ProductsComponent}
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  {path:'order',component:OrdersComponent},
+  {path:'home',component:HomeComponent},
+  {path:'products',component:ProductsComponent},
+  // {path:'admin-dashboard',component:AdminDashboardComponent},
 ];
 
 @NgModule({
